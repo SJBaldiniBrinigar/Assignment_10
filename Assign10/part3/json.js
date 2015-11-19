612549b6fce96974e8216d8d32984346cec6394d
SBaldiniBrinigar <baldinisj@aol.com>
2015-11-19 15:58:10 -0800

    Assignment_10 commit

---
+++ Assign10/part3/json.js
@@ -0,0 +1,11 @@
+
+{"zooAnimals":[
+    {"breed":"Elephant", "name":"Dumbo", "age":"37yrs", "location":"cage 51"},
+    {"breed":"Tiger", "name":"Stripes", "age":"8yrs", "location":"cage 21"},
+    {"breed":"Lion", "name":"Leo", "age":"5yrs", "location":"cage 25"},
+    {"breed":"Giraffe", "name":"Sky", "age":"18yrs", "location":"cage 34"},
+    {"breed":"Gorilla", "name":"Big Foot", "age":"28yrs", "location":"cage 47"},
+    {"breed":"Hippopotamus", "name":"Happy", "age":"33yrs", "location":"cage 8"}
+    
+]}
+
